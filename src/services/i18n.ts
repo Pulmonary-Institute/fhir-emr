e08227e32d89fa05c1fd501397f5cede2de911cb
AK5000M <azmatkassayev0032@outlook.com>
2025-02-06 20:27:19 +0500

    removed Ru in language

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,28 +1,24 @@
 import { i18n } from '@lingui/core';
-import { en, es, ru } from 'make-plural/plurals';
+import { en, es } from 'make-plural/plurals';
 
 import { messages as enMessages } from 'src/locale/en/messages';
 import { messages as esMessages } from 'src/locale/es/messages';
-import { messages as ruMessages } from 'src/locale/ru/messages';
 
-export type LocaleCode = 'en' | 'es' | 'ru';
+export type LocaleCode = 'en' | 'es';
 
 const localMap = {
     en: enMessages,
     es: esMessages,
-    ru: ruMessages,
 };
 
 export const locales = {
     en: 'English',
     es: 'Español',
-    ru: 'Русский',
 };
 
 i18n.loadLocaleData({
     en: { plurals: en },
     es: { plurals: es },
-    ru: { plurals: ru },
 });
 
 export const getCurrentLocale = () => {
