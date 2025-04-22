1e06b31b7d2b1e08c8e22c48b7d5b500bcdd8d81
rrabreu2425 <rrabreu2425@gmail.com>
2025-04-22 16:36:46 -0400

    add badge with icon to show total of filters applied

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,5 +1,6 @@
 import { Trans } from '@lingui/macro';
-import { Button } from 'antd';
+import { Button, Badge } from 'antd';
+import { FilterOutlined } from '@ant-design/icons';
 
 import { SearchBarColumn } from './SearchBarColumn';
 import { S } from './styles';
@@ -18,9 +19,21 @@ export function SearchBar(props: SearchBarProps) {
         () => columnsFilterValues.filter((filter) => isSearchBarFilter(filter)),
         [JSON.stringify(columnsFilterValues)],
     );
+    const appliedFiltersCount = useMemo(() => {
+        return searchBarFilterValues.reduce((count, filter) => {
+            // Considerar que un filtro está aplicado si tiene algún valor
+            if (filter.value !== undefined && filter.value !== null && filter.value !== '') {
+                return count + 1;
+            }
+            return count;
+        }, 0);
+    }, [searchBarFilterValues]);
     return (
         <>
             <S.SearchBar $showInDrawerOnMobile={showInDrawerOnMobile}>
+                <Badge count={appliedFiltersCount} size="small">
+                    <FilterOutlined style={{ fontSize: '16px', marginLeft: '8px' }} />
+                </Badge>
                 <S.LeftColumn>
                     {searchBarFilterValues.map((columnFilterValue) => (
                         <SearchBarColumn
