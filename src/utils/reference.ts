6ab5ecfc7f1e55ff2ca4dad456db50b1c4ac5a4f
AK5000M <168441817+AK5000M@users.noreply.github.com>
2025-04-24 20:22:06 +0500

    fix minor type error in resolveReference (#8)

--- src/utils/reference.ts
+++ src/utils/reference.ts
@@ -17,6 +17,6 @@ export function getFHIRReferenceResourceId(reference?: Reference) {
 }
 
 export function resolveReference<R extends Resource = never>(bundle: Bundle, reference: Reference): R | undefined {
-    return bundle.entry?.find((entry) => reference.reference && entry.fullUrl?.endsWith(reference.reference))
+    return bundle.entry?.find((entry) => reference?.reference && entry.fullUrl?.endsWith(reference?.reference))
         ?.resource as R | undefined;
 }
